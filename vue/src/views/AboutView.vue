13d80c34c7ebbebf3ad344e20eedfcfb9ee30c28
rimutaka <max@onebro.me>
2024-11-05 14:13:29 +1300

    Added About page draft

--- vue/src/views/AboutView.vue
+++ vue/src/views/AboutView.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h3>Privacy</h3>
+    <p>No data from this app is shared with any third party.</p>
+    <p>There is no client-side tracking or third-party scripts loaded into the browser.</p>
+    <p>The authentication is done through <a href="https://auth0.com">Auth0</a>.</p>
+    <p>Source code: <a href="https://github.com/rimutaka/bitesized">https://github.com/rimutaka/bitesized</a>.</p>
+
+    <h3>Scoring and leaderboards</h3>
+    <p>Individual results and performance stats are only available to that individual.</p>
+    <p>The purpose of this app is to help us learn through questions.</p>
+
+    <h3>Feedback</h3>
+    <p>Feedback is welcome. The best way to get in touch is to <a href="mailto:max@onebro.me">email the developer</a>.</p>
+    <p>If you run into bugs, press <em>Ctrl+Shift+I</em> and copy-paste the console output into the email.<br />
+      Your help with making the site better will be much appreciated.</p>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
