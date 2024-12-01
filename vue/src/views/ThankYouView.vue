32f646138ddfe2904126da08851d4c1c855d0469
rimutaka <max@onebro.me>
2024-12-01 22:21:13 +1300

    Added an icon to Thank you page

--- vue/src/views/ThankYouView.vue
+++ vue/src/views/ThankYouView.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="about">
-    <h3>Thank you for your generous support</h3>
-    <p>Someone will be in touch in the next few days to update you on the progress.</p>
-    <p>We will confirm the attribution details with you before the questions are published.</p>
-    <p>Feel free to contact the maintainer on <a href="mailto:max@bitesized.info">max@bitesized.info</a> if you have any question or feedback.</p>
-    <p>Thank you again for supporting this project and all the people who will learn through the questions you sponsored.</p>
+  <div>
+    <div class="about">
+      <h3>Thank you for your generous support</h3>
+      <p>Someone will be in touch in the next few days to update you on the progress.</p>
+      <p>We will confirm the attribution details with you before the questions are published.</p>
+      <p>Feel free to contact the maintainer at <a href="mailto:max@bitesized.info">max@bitesized.info</a> if you have any questions or feedback.</p>
+      <p>Thank you again for supporting this project and all the people who will learn from the questions you sponsored.</p>
+    </div>
+    <div class="my-12 subdued">
+      <div class="mx-auto w-28 h-28 contributor-icon subdued-image"></div>
+    </div>
   </div>
 </template>
