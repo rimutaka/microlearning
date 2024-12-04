e4adf3bbb6a6f728407ea8847190d25b2b366417
rimutaka <max@onebro.me>
2024-12-05 11:54:02 +1300

    Reworded Gift a question

--- vue/src/components/SponsorshipCTA.vue
+++ vue/src/components/SponsorshipCTA.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="max-w-screen-sm">
     <h1 class="text-balance mb-4 text-slate-700 dark:text-slate-200">Gift a question</h1>
-    <p class=" text-start text-slate-700 dark:text-slate-200">As a token of appreciation from the software engineering community for sponsoring a free educational resource,
+    <p class=" text-start text-slate-700 dark:text-slate-200">As a token of appreciation from the software engineering community for supporting a free educational resource,
       we will add</p>
     <ul class="" id="signup-pitch">
       <li><i class="pi pi-heart"></i>a permanent <em>thank you</em> message</li>
