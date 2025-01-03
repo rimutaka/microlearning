afd8f63fc999c09b1deb3eebabd0cde7f2b91c90
rimutaka <max@onebro.me>
2025-01-04 11:05:24 +1300

    Sponsorship CTA list bug fix

--- vue/src/components/SponsorshipCTA.vue
+++ vue/src/components/SponsorshipCTA.vue
@@ -3,7 +3,7 @@
     <h1 class="text-balance mb-4 text-slate-700 dark:text-slate-200">Gift a question</h1>
     <p class=" text-start text-slate-700 dark:text-slate-200">As a token of appreciation from the software engineering community for supporting a free educational resource,
       we will add</p>
-    <ul class="" id="signup-pitch">
+    <ul class="signup-pitch w-fit mx-auto">
       <li><i class="pi pi-heart"></i>a permanent <em>thank you</em> message</li>
       <li><i class="pi pi-sparkles"></i>to your sponsored questions</li>
       <li><i class="pi pi-desktop"></i>shown on webpages and in emails</li>
